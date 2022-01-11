554ddf13cff4d51c79a8c39bc726c739818f753c
p1chiu <p1chiu@ryerson.ca>
2022-01-11 13:11:58 -0500

    fixed babelrc bug
    added custom babelrc config

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function Home() {
   return (
+    <ChakraProvider>
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -65,5 +67,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </ChakraProvider>
   )
 }
